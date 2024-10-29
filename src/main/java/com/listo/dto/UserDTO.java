368453ebe7af8c1f863b6ddf86cf25f304c2b078
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-28 22:58:52 -0300

    feat: create User entity and dto

---
+++ src/main/java/com/listo/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.listo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.listo.model.User}
+ */
+public record UserDTO(
+        Long id,
+
+        @NotBlank(message = "Name cannot be blank or empty")
+        String name,
+
+        @NotBlank(message = "Login cannot be blank or empty")
+        String login,
+
+        @NotBlank(message = "Password cannot be blank or empty")
+        String password) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}

8e65b0b58d4e302607441e3671d2ecc0281e1db4
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-10-28 23:29:17 -0300

    feat: create UserResponseDTO

---
+++ src/main/java/com/listo/dto/user/UserResponseDTO.java
@@ -0,0 +1,34 @@
+package com.listo.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.listo.model.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.listo.model.User}
+ */
+@JsonPropertyOrder({"id", "name", "login", "password"})
+public record UserResponseDTO(
+
+        @JsonProperty("id")
+        Long id,
+
+        @JsonProperty("name")
+        String name,
+
+        @JsonProperty("login")
+        String login,
+
+        @JsonProperty("password")
+        String password) implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public UserResponseDTO(User user) {
+        this(user.getId(), user.getName(), user.getLogin(), user.getPassword());
+    }
+}

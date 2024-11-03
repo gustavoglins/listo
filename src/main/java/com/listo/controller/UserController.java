715b6c27046a4cb3b7807dcd30b3986b95e4e7c5
gustavoglins <gustavoguimaraes.241025@gmail.com>
2024-11-03 17:54:47 -0300

    feat: documenting UserController

--- src/main/java/com/listo/controller/UserController.java
+++ src/main/java/com/listo/controller/UserController.java
@@ -3,6 +3,10 @@ package com.listo.controller;
 import com.listo.dto.user.UserRequestDTO;
 import com.listo.dto.user.UserResponseDTO;
 import com.listo.services.UserService;
+import com.listo.utils.CustomMediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping(value = "/api/user")
+@Tag(name = "User Management", description = "Endpoints for managing users, including creating, updating, retrieving, and deleting user information.")
 public class UserController {
 
     private final UserService service;
@@ -20,32 +25,99 @@ public class UserController {
         this.service = service;
     }
 
-    @PostMapping
+    @PostMapping(consumes = {CustomMediaType.APPLICATION_JSON},
+                 produces = {CustomMediaType.APPLICATION_JSON})
+    @Operation(
+            summary = "Create a new user",
+            description = "Create a new user",
+            tags = {"User Management"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201"),
+                    @ApiResponse(description = "Bad Request", responseCode = "400"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Conflict", responseCode = "409"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422"),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "501")
+            }
+    )
     public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid UserRequestDTO requestDTO) {
         UserResponseDTO createdUser = service.create(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
-    @PutMapping
+    @PutMapping(consumes = {CustomMediaType.APPLICATION_JSON},
+                produces = {CustomMediaType.APPLICATION_JSON})
+    @Operation(
+            summary = "Update a user",
+            description = "Update a user",
+            tags = {"User Management"},
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200"),
+                    @ApiResponse(description = "Bad Request", responseCode = "400"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Not Found", responseCode = "404"),
+                    @ApiResponse(description = "Conflict", responseCode = "409"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422"),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "501")
+            }
+    )
     public ResponseEntity<UserResponseDTO> update(@RequestBody @Valid UserRequestDTO requestDTO) {
         UserResponseDTO createdUser = service.update(requestDTO);
         return ResponseEntity.ok(createdUser);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> findById(@PathVariable long id){
+    @GetMapping(value = "/{id}",
+                produces = {CustomMediaType.APPLICATION_JSON})
+    @Operation(
+            summary = "Find a user by ID",
+            description = "Find a user by ID",
+            tags = {"User Management"},
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Not Found", responseCode = "404"),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "501")
+            }
+    )
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable long id) {
         UserResponseDTO foundUser = service.findById(id);
         return ResponseEntity.ok(foundUser);
     }
 
-    @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> listAll(){
+    @GetMapping(produces = {CustomMediaType.APPLICATION_JSON})
+    @Operation(
+            summary = "List all registered users",
+            description = "List all registered users",
+            tags = {"User Management"},
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "5001")
+            }
+    )
+    public ResponseEntity<List<UserResponseDTO>> listAll() {
         List<UserResponseDTO> foundUsers = service.listAll();
         return ResponseEntity.ok(foundUsers);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id){
+    @Operation(
+            summary = "Delete a user by ID",
+            description = "Delete a user by ID",
+            tags = {"User Management"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Not Found", responseCode = "404"),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "501")
+            }
+    )
+    public void delete(@PathVariable long id) {
         service.delete(id);
     }
 }
